d8201b47bb3de6522d8e40d0088d349fac43e65d
Farhan Malik <farhanmalik01@outlook.com>
2015-11-18 23:00:39 +0300

    test email steeings again

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,8 @@ class ContactsController < ApplicationController
 			email = params[:contact][:email]
 			body = params[:contact][:comments]
 
-			ContactMailer.contact_email(name, email, body).deliver
+			ContactMailer.contacts_email(name, email, body).deliver
+
 			flash[:success] = 'Message Sent.'
 			redirect_to new_contact_path
 		else
