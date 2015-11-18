995d68c134687a2fc45c5c948eb456e36f598469
Farhan Malik <farhanmalik01@outlook.com>
2015-11-19 01:40:45 +0300

    pungi suda

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,7 @@ class ContactsController < ApplicationController
 			email = params[:contact][:email]
 			body = params[:contact][:comments]
 
-			ContactMailer.contacts_email(name, email, body).deliver
+			ContactMailer.contact_email(name, email, body).deliver
 
 			flash[:success] = 'Message Sent.'
 			redirect_to new_contact_path
